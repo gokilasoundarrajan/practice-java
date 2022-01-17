c9ea4b929f610402bd5ec578cca9fefa89f8a860
gokilasoundarrajan <goki.soundarrajan@gmail.com>
2022-01-17 10:35:56 +0400

    17/1/21: Reverse String

--- src/problems/ReverseString.java
+++ src/problems/ReverseString.java
@@ -5,10 +5,41 @@ public class ReverseString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = "Hello";
+		System.out.println(usingCharAt(str));
+		System.out.println(usingCharArray(str));
+		System.out.println(usingStringBuilder(str));
+		System.out.println(usingStringBuffer(str));
+
+	}
+	
+	static String usingCharAt(String str) {
 		String newStr = "";
 		for(int i=str.length()-1;i>=0;i--)
 			newStr += str.charAt(i);
-		System.out.println(newStr);
+		return newStr;
 	}
-
+	
+	static String usingCharArray(String str){
+		char newCharArr[] = new char[str.length()];
+		char[] charArr = str.toCharArray(); 
+        for (int i = charArr.length - 1, j = 0; i >= 0; i--,j++)
+        	newCharArr[j] = charArr[i];
+        return String.valueOf(newCharArr);
+	}
+	
+	
+	static String usingStringBuilder(String str) {
+		StringBuilder input = new StringBuilder();		 
+        // append a string into StringBuilder input1
+        input.append(str);
+        // reverse StringBuilder input1
+        input.reverse();
+        return input.toString();
+	}
+	
+	static String usingStringBuffer(String str) {
+		StringBuffer strBuf = new StringBuffer(str);
+		strBuf.reverse();
+		return strBuf.toString();
+	}
 }
